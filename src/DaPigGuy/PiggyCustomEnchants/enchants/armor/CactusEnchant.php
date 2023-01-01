dfcabdf2c63129cf9f378b26a6c9e86e54fc5a53
Ihsan48 <101813808+Ihsan48@users.noreply.github.com>
2023-01-01 14:44:19 +0700

    Test

--- src/DaPigGuy/PiggyCustomEnchants/enchants/armor/CactusEnchant.php
+++ src/DaPigGuy/PiggyCustomEnchants/enchants/armor/CactusEnchant.php
@@ -8,6 +8,7 @@ use DaPigGuy\PiggyCustomEnchants\enchants\CustomEnchant;
 use DaPigGuy\PiggyCustomEnchants\enchants\TickingEnchantment;
 use DaPigGuy\PiggyCustomEnchants\utils\AllyChecks;
 use brokiem\snpc\entity\CustomHuman;
+use brokiem\snpc\entity\BaseNPC;
 use pocketmine\entity\Living;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\entity\EntityDamageEvent;
@@ -31,7 +32,7 @@ class CactusEnchant extends TickingEnchantment
     public function tick(Player $player, Item $item, Inventory $inventory, int $slot, int $level): void
     {
         foreach ($player->getWorld()->getNearbyEntities($player->getBoundingBox()->expandedCopy(1, 0, 1), $player) as $entity) {
-            if ($entity instanceof Living && !AllyChecks::isAlly($player, $entity) && !$entity instanceof CustomHuman) {
+            if ($entity instanceof Living && !AllyChecks::isAlly($player, $entity) && !$entity instanceof CustomHuman || !$entity instanceof BaseNPC) {
                 $ev = new EntityDamageByEntityEvent($player, $entity, EntityDamageEvent::CAUSE_CONTACT, 1);
                 $entity->attack($ev);
             }
